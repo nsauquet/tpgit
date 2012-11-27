894748dac23991793be71f86c51c1ed983de16a8
nicolas <21207571@unicaen.fr>
2012-11-27 10:28:43 +0100

    Initialisation GIT + Ajout fichiers

---
+++ phpmyadmin/config-db.php
@@ -0,0 +1,19 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+## Wed, 17 Oct 2012 11:30:24 +0200
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+$dbuser='phpmyadmin';
+$dbpass='asrsi';
+$basepath='';
+$dbname='phpmyadmin';
+$dbserver='';
+$dbport='';
+$dbtype='mysql';
